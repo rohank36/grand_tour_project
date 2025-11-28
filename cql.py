a55655bc3234e259cfaf351d6e9c9a0d7ea13387
modanesh <mohamad4danesh@gmail.com>
2025-11-28 18:20:35 -0500

    cql stable

--- cql.py
+++ cql.py
@@ -33,18 +33,19 @@ class TrainConfig:
     device: str = "cuda"
     #env: str = "halfcheetah-medium-expert-v2"  # OpenAI gym environment name
     seed: int = 0  # Sets Gym, PyTorch and Numpy seeds
+    eval_seed: int = 27 # sets seed for online eval
     #eval_freq: int = int(5e3)  # How often (time steps) we evaluate
-    eval_freq: int = int(500) 
+    eval_freq: int = int(1000) 
     n_episodes: int = 10  # How many episodes run during evaluation
     #max_timesteps: int = int(1e6)  # Max time steps to run environment
-    max_timesteps: int = int(1e4) # FOR TESTING
+    max_timesteps: int = int(5e3) # FOR TESTING
     checkpoints_path: Optional[str] = None  # Save path
     load_model: str = ""  # Model load file name, "" doesn't load
     buffer_size: int = 2_000_000  # Replay buffer size
     batch_size: int = 256  # Batch size for all networks
     discount: float = 0.99  # Discount factor
     alpha_multiplier: float = 1.0  # Multiplier for alpha in loss
-    use_automatic_entropy_tuning: bool = True  # Tune entropy
+    use_automatic_entropy_tuning: bool = False  # Tune entropy
     backup_entropy: bool = False  # Use backup entropy
     policy_lr: float = 3e-5  # Policy learning rate
     qf_lr: float = 3e-4  # Critics learning rate
@@ -52,20 +53,20 @@ class TrainConfig:
     target_update_period: int = 1  # Frequency of target nets updates
     cql_n_actions: int = 10  # Number of sampled actions
     cql_importance_sample: bool = True  # Use importance sampling
-    cql_lagrange: bool = False  # Use Lagrange version of CQL
-    cql_target_action_gap: float = -1.0  # Action gap
+    cql_lagrange: bool = True  # Use Lagrange version of CQL
+    cql_target_action_gap: float = 1.0 # Action gap
     cql_temp: float = 1.0  # CQL temperature
-    cql_alpha: float = 10.0  # Minimal Q weight
-    cql_max_target_backup: bool = False  # Use max target backup
-    cql_clip_diff_min: float = -np.inf  # Q-function lower loss clipping
-    cql_clip_diff_max: float = np.inf  # Q-function upper loss clipping
+    cql_alpha: float = 1.0  # Minimal Q weight
+    cql_max_target_backup: bool = True  # Use max target backup
+    cql_clip_diff_min: float = -100  # Q-function lower loss clipping
+    cql_clip_diff_max: float = 100  # Q-function upper loss clipping
     orthogonal_init: bool = True  # Orthogonal initialization
     normalize: bool = True  # Normalize states
     normalize_reward: bool = False  # Normalize reward
     q_n_hidden_layers: int = 3  # Number of hidden layers in Q networks
-    bc_steps: int = int(0)  # Number of BC steps at start
-    reward_scale: float = 5.0  # Reward scale for normalization
-    reward_bias: float = -1.0  # Reward bias for normalization
+    bc_steps: int = int(5e4)  # Number of BC steps at start
+    reward_scale: float = 1.0  # Reward scale for normalization
+    reward_bias: float = 0.0  # Reward bias for normalization
     policy_log_std_multiplier: float = 1.0  # Stochastic policy std multiplier
     
     project: str = "grand_tour"  # wandb project name
@@ -369,6 +370,7 @@ class TanhGaussianPolicy(nn.Module):
         self.log_std_offset = Scalar(log_std_offset)
         self.tanh_gaussian = ReparameterizedTanhGaussian(no_tanh=no_tanh)
 
+    """
     def log_prob(
         self, observations: torch.Tensor, actions: torch.Tensor
     ) -> torch.Tensor:
@@ -378,6 +380,29 @@ class TanhGaussianPolicy(nn.Module):
         mean, log_std = torch.split(base_network_output, self.action_dim, dim=-1)
         log_std = self.log_std_multiplier() * log_std + self.log_std_offset()
         _, log_probs = self.tanh_gaussian(mean, log_std, False)
+        return log_probs
+    """
+    
+    def log_prob(
+        self, 
+        observations: torch.Tensor, 
+        actions: torch.Tensor = None
+    ) -> torch.Tensor:
+        if actions.ndim == 3 and observations.ndim == 2:
+            observations = extend_and_repeat(observations, 1, actions.shape[1])
+
+        base_network_output = self.base_network(observations)
+        mean, log_std = torch.split(base_network_output, self.action_dim, dim=-1)
+        log_std = self.log_std_multiplier() * log_std + self.log_std_offset()
+
+        # env-scale -> tanh-space
+        if not self.no_tanh:
+            actions_tanh = (actions / self.max_action).clamp(-0.999999, 0.999999)
+        else:
+            actions_tanh = actions
+
+        # This calls ReparameterizedTanhGaussian.log_prob and calculates the log prob of the dataset actions under the current policy distribution
+        log_probs = self.tanh_gaussian.log_prob(mean, log_std, actions_tanh)
         return log_probs
 
     def forward(
@@ -568,7 +593,8 @@ class ContinuousCQL:
     ) -> torch.Tensor:
         if self.total_it <= self.bc_steps:
             log_probs = self.actor.log_prob(observations, actions)
-            policy_loss = (alpha * log_pi - log_probs).mean()
+            #policy_loss = (alpha * log_pi - log_probs).mean()
+            policy_loss = -log_probs.mean()
         else:
             q_new_actions = torch.min(
                 self.critic_1(observations, new_actions),
@@ -1106,11 +1132,6 @@ def train(config: TrainConfig):
         "obs/hist": wandb.Histogram(ds_observations.flatten()),
 
         # Per-dimension stats 
-        #**{f"obs/mean_dim_{i}": float(obs_mean[i]) for i in range(state_dim)},
-        #**{f"obs/std_dim_{i}":  float(obs_std[i])  for i in range(state_dim)},
-        #**{f"obs/min_dim_{i}":  float(obs_min[i])  for i in range(state_dim)},
-        #**{f"obs/max_dim_{i}":  float(obs_max[i])  for i in range(state_dim)},
-
         "obs/mean_per_dim_plot":  obs_mean_plot,
         "obs/std_per_dim_plot":   obs_std_plot,
         "obs/min_per_dim_plot":   obs_min_plot,
@@ -1130,11 +1151,6 @@ def train(config: TrainConfig):
         "action/hist": wandb.Histogram(ds_actions.flatten()),
 
         # Per-dimension stats
-        #**{f"action/mean_dim_{i}": float(act_mean[i]) for i in range(action_dim)},
-        #**{f"action/std_dim_{i}":  float(act_std[i])  for i in range(action_dim)},
-        #**{f"action/min_dim_{i}":  float(act_min[i])  for i in range(action_dim)},
-        #**{f"action/max_dim_{i}":  float(act_max[i])  for i in range(action_dim)},
-
         "action/mean_per_dim_plot": act_mean_plot,
         "action/std_per_dim_plot":  act_std_plot,
         "action/min_per_dim_plot":  act_min_plot,
@@ -1143,8 +1159,7 @@ def train(config: TrainConfig):
     },step=0)
     # -------------------------------------------
 
-    evaluations = []
-    online_eval = OnlineEval(task_name="anymal_d_flat",seed=27)
+    online_eval = OnlineEval(task_name="anymal_d_flat",seed=config.eval_seed)
     start_time = time.time()
     #for t in range(int(config.max_timesteps)):
     for t in tqdm(range(int(config.max_timesteps)), desc="Training CQL"):
@@ -1154,33 +1169,10 @@ def train(config: TrainConfig):
         wandb.log(log_dict, step=trainer.total_it)
         # Evaluate episode
         
+        """
         if (t + 1) % config.eval_freq == 0:
-        #if t == config.max_timesteps+1: # FOR TESTING
-            """
-            eval_scores = eval_actor(
-                env,
-                actor,
-                device=config.device,
-                n_episodes=config.n_episodes,
-                seed=config.seed,
-            )
-            """
-            eval_score,n_eps_evaluated,scaled_rew_terms_avg = online_eval.eval_actor_isaac(
-                                            actor=actor,
-                                            #task_name="anymal_c_flat",
-                                            device=config.device,
-                                        )
-            #eval_score = eval_scores.mean()
-            #normalized_eval_score = env.get_normalized_score(eval_score) * 100.0
-            #evaluations.append(normalized_eval_score)
-            evaluations.append(eval_score)
+            eval_score,n_eps_evaluated,scaled_rew_terms_avg = online_eval.eval_actor_isaac(actor=actor,device=config.device,)
             print("---------------------------------------")
-            """
-            print(
-                f"Evaluation over {config.n_episodes} episodes: "
-                f"{eval_score:.3f} , D4RL score: {normalized_eval_score:.3f}"
-            )
-            """
             print(
                 f"Evaluation over {n_eps_evaluated} episodes: {eval_score:.3f} "
             )
@@ -1191,14 +1183,6 @@ def train(config: TrainConfig):
                     trainer.state_dict(),
                     os.path.join(config.checkpoints_path, f"checkpoint_{t}.pt"),
                 )
-            
-            """
-            wandb.log(
-                #{"d4rl_normalized_score": normalized_eval_score},
-                {"isaac_reward": eval_score},
-                step=trainer.total_it,
-            )
-            """
             wandb.log(
                 {
                     "isaac_reward": eval_score,
@@ -1207,28 +1191,11 @@ def train(config: TrainConfig):
                 },
                 step=trainer.total_it,
             )
-
-        #if t==2: break # FOR TESTING
-
+        """
     end_time = time.time()
-    training_time_minutes = (end_time - start_time) / 60
-    np_eval = np.array(evaluations) 
-    print(f"\n\n\n")
+    training_time_minutes = (end_time - start_time) / 60 
+    print(f"\n\n")
     print(f"Training duration: {training_time_minutes:.2f} minutes")
-    print(f"MIN online eval score: {np_eval.min()}")
-    print(f"MAX online eval score: {np_eval.max()}")
-    print(f"MEAN online eval score: {np_eval.mean()}")
-
-    """
-    #FOR TESTING:
-    eval_score,n_eps_evaluated = online_eval.eval_actor_isaac(
-                                            actor=actor,
-                                            #task_name="anymal_c_flat",
-                                            device=config.device,
-                                        )
-    print(f"\n\nEvaluation over {n_eps_evaluated} episodes: {eval_score:.3f}")
-    """
-
 
 if __name__ == "__main__":
     train()
